87f3f9dab8af16eafc535901379e1d6a11e8f39c
therahulvaish <vaishr238@gmail.com>
2023-03-13 00:34:08 +0530

    added click event on buttons, game is functioning

--- script2.js
+++ script2.js
@@ -1,40 +1,87 @@
+// Variables 
+
+const userScore = document.querySelector('#user-score');
+const compScore = document.querySelector('#comp-score');
+const displayResult = document.querySelector('#result');
+
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorsButton = document.querySelector('#scissors');
+
+
+
+// Game Play Function
+
 function getComputerChoice() {
-    let choices = ["rock", "paper", "scissors"];
-    let index = Math.floor(Math.random() * choices.length);
-    return choices[index];
+        let choices = ["rock", "paper", "scissors"];
+        let index = Math.floor(Math.random() * choices.length);
+        return choices[index];
+    }
+
+
+function buttonChoice(playerSelection) {
+    const computerSelection = getComputerChoice();
+    let result = playerSelection+computerSelection;
+
+    switch (result) {
+
+        case 'rockrock' :
+            displayResult.innerText = "Tt's a tie!!";
+            displayResult.style.color = 'orange';
+            break;
+    
+        case 'rockpaper' :
+            displayResult.innerText = "Computer Won!!";
+            displayResult.style.color = 'red';
+            break;
+    
+        case 'rockscissors' :
+            displayResult.innerText = "Player Won!!";
+            displayResult.style.color = 'green';
+            break;
+    
+        case 'paperpaper' :
+            displayResult.innerText = "Tt's a tie!!";
+            displayResult.style.color = 'orange';
+            break;
+    
+        case 'paperrock' :
+            displayResult.innerText = "Player Won!!";
+            displayResult.style.color = 'green';
+            break;
+    
+        case 'paperscissors' :
+            displayResult.innerText = "Computer Won!!";
+            displayResult.style.color = 'red';
+            break;
+    
+        case 'scissorsscissors' :
+            displayResult.innerText = "Tt's a tie!!";
+            displayResult.style.color = 'orange';
+            break;
+        
+        case 'scissorsrock' :
+            displayResult.innerText = "Computer Won!!";
+            displayResult.style.color = 'red';
+            break;
+
+        case 'scissorspaper' :
+            displayResult.innerText = "Player Won!!";
+            displayResult.style.color = 'green';
+            break;
+
+    }
+
 }
 
-function gamePlay(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        console.log("It's a tie")
-    }
+// Click Event on choices
 
-    else if (computerSelection == "rock" && playerSelection == "paper") {
-        console.log("Player Won!")
-    }
-
-    else if (computerSelection == "paper" && playerSelection == "scissors") {
-        console.log("Player Won!")
-    }
-
-    else if (computerSelection == "scissors" && playerSelection == "rock") {
-        console.log("Player Won!")
-    }
-
-    else if (computerSelection == "rock" && playerSelection == "scissors") {
-        console.log("Computer Won!")
-    }
-
-    else if (computerSelection == "paper" && playerSelection == "rock") {
-        console.log("Computer Won!")
-    }
-
-    else if (computerSelection == "scissors" && playerSelection == "paper") {
-        console.log("Computer Won!")
-    }
-
-}
-
-for (i = 0; i < 5; i++) {
-    gamePlay(prompt("Enter your choice").toLowerCase(), getComputerChoice());
-}
+    rockButton.addEventListener('click', function () {
+        buttonChoice('rock');
+    })
+    paperButton.addEventListener('click', function () {
+        buttonChoice('paper');
+    })
+    scissorsButton.addEventListener('click', function () {
+        buttonChoice('scissors');
+    })
